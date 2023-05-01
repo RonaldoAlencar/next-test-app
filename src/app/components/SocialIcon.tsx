a96b9db7266844404a67add583c4a25d15cc7367
Ronaldo <ronaldom32@gmail.com>
2023-05-01 14:06:03 -0300

    feat: add animation on social icon login

--- src/app/components/SocialIcon.tsx
+++ src/app/components/SocialIcon.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import Spinner from "./Spinner"
+
 interface SocialIconProps {
   label: string,
   icon: React.ReactNode,
@@ -5,14 +8,22 @@ interface SocialIconProps {
 }
 
 export default function SocialIcon(props: SocialIconProps) {
+  const [showSpinner, setShowSpinner] = useState(false)
+
+  const handleClick = () => {
+    setShowSpinner(true)
+    props.onClick()
+  }
+
   return (
     <div 
-      className="flex flex-col items-center justify-center gap-1 cursor-pointer hover:bg-gray-100 p-2 rounded-md"
-      onClick={() => props.onClick()}
+      className="flex flex-col items-center justify-center gap-1 cursor-pointer hover:bg-gray-100 p-2 rounded-md hover:scale-110 transition duration-200 ease-in-out transform hover:shadow-lg"
+      onClick={handleClick}
     >
       <div 
         className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-100"
       >
+        <Spinner show={showSpinner} />
         {props.icon}
       </div>
       <span 
