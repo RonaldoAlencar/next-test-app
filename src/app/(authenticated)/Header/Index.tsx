d25ee012954bd3999f0e86a6bba929e049ad1b12
Ronaldo <ronaldom32@gmail.com>
2023-05-04 22:59:21 -0300

    fix: fixed dark theme

--- src/app/(authenticated)/Header/Index.tsx
+++ src/app/(authenticated)/Header/Index.tsx
@@ -34,15 +34,15 @@ export default function Header({ session }: HeaderProps) {
   const [showHamburgerMenu, setShowHamburgerMenu] = useState(false)
 
   return (
-    <nav className="bg-white border-gray-200">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" className="flex items-center">
         <Image width={50} height={50} src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap">Mural de Mensagens</span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Mural de Mensagens</span>
       </a>
 
       <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <LinkItem href="/" text="Início" />
           <LinkItem href="/about" text="Sobre" selected={true} />
           <LinkItem href="/my-account" text="Minha Conta" />
@@ -52,7 +52,7 @@ export default function Header({ session }: HeaderProps) {
       <div className="flex items-center md:order-2">
           <button 
             type="button" 
-            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 group focus:outline-none" 
+            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 group focus:outline-none" 
             onMouseEnter={() => setShowSubMenu(true)}
           >
             <Image 
@@ -73,7 +73,7 @@ export default function Header({ session }: HeaderProps) {
 
           <button 
             type="button" 
-            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             onClick={() => setShowHamburgerMenu(!showHamburgerMenu)}
           >
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
