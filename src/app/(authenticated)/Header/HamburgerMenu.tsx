fb4838093c06cc8736ae39475fec63ddcef74c45
Ronaldo <ronaldom32@gmail.com>
2023-05-04 23:07:41 -0300

    fix: dark theme

--- src/app/(authenticated)/Header/HamburgerMenu.tsx
+++ src/app/(authenticated)/Header/HamburgerMenu.tsx
@@ -15,8 +15,8 @@ function LinkItem ({ href, text, selected }: LinkItemProps) {
         href={href}
         className={
           selected 
-          ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-          : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+          ? "block py-2 pl-3 pr-4 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:text-blue-500"
+          : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
         }
       >
         {text}
@@ -28,7 +28,7 @@ function LinkItem ({ href, text, selected }: LinkItemProps) {
 export default function HamburgerMenu({ show }: HamburgerMenuProps) {
   return (
     <div className={show ? "items-center justify-between w-full md:flex md:w-auto md:order-1" : "hidden"}>
-      <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
         <LinkItem href="/" text="Início" />
         <LinkItem href="/about" text="Sobre" selected={true} />
         <LinkItem href="/my-account" text="Minha Conta" />
