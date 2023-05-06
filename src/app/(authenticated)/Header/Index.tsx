784754656b4c54eb3d9ac56e20c9ecea55b104c5
Ronaldo <ronaldom32@gmail.com>
2023-05-06 12:37:44 -0300

    fix: fixed css class

--- src/app/(authenticated)/Header/Index.tsx
+++ src/app/(authenticated)/Header/Index.tsx
@@ -38,11 +38,15 @@ export default function Header({ session }: HeaderProps) {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" className="flex items-center">
         <Image width={50} height={50} src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Mural de Mensagens</span>
+        <span 
+          className="self-center text-2xl font-semibold whitespace-nowrap text-white"
+        >
+          Mural de Mensagens
+        </span>
       </a>
 
       <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
+        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
           <LinkItem href="/" text="Início" />
           <LinkItem href="/about" text="Sobre" selected={true} />
           <LinkItem href="/my-account" text="Minha Conta" />
@@ -52,7 +56,7 @@ export default function Header({ session }: HeaderProps) {
       <div className="flex items-center md:order-2">
           <button 
             type="button" 
-            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 focus:ring-gray-600 group focus:outline-none" 
+            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-600 group focus:outline-none" 
             onMouseEnter={() => setShowSubMenu(true)}
           >
             <Image 
@@ -73,7 +77,7 @@ export default function Header({ session }: HeaderProps) {
 
           <button 
             type="button" 
-            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-1 text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 text-gray-400 focus:ring-gray-600"
             onClick={() => setShowHamburgerMenu(!showHamburgerMenu)}
           >
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
--- src/app/(authenticated)/Header/SettingsSubMenu.tsx
+++ src/app/(authenticated)/Header/SettingsSubMenu.tsx
@@ -19,25 +19,25 @@ export default function SettingsSubMenu({ show, setShow, userEmail, userName }: 
       className={show ? "relative" : "relative hidden"}
       onMouseLeave={() => setShow(false)}
     >
-      <div className="z-50 my-4 text-base list-none divide-y divide-gray-100 rounded-lg shadow bg-gray-700 divide-gray-600 absolute top-1 right-0 w-56">
+      <div className="z-50 my-4 text-base list-none divide-y rounded-lg shadow bg-gray-700 divide-gray-600 absolute top-1 right-0 w-56">
         <div className="px-4 py-3">
-          <span className="block text-sm text-gray-900 text-white">{userName}</span>
-          <span className="block text-sm  text-gray-500 truncate text-gray-400">{userEmail}</span>
+          <span className="block text-sm text-gray-200">{userName}</span>
+          <span className="block text-sm  text-gray-500 truncate">{userEmail}</span>
         </div>
         <ul className="py-2" aria-labelledby="user-menu-button">
           <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-100 hover:bg-gray-600 text-gray-200 hover:text-white">Dashboard</a>
+            <a href="#" className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white">Dashboard</a>
           </li>
           <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-100 hover:bg-gray-600 text-gray-200 hover:text-white">Settings</a>
+            <a href="#" className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white">Settings</a>
           </li>
           <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-100 hover:bg-gray-600 text-gray-200 hover:text-white">Earnings</a>
+            <a href="#" className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white">Earnings</a>
           </li>
           <li>
             <a 
               href="#"
-              className="block px-4 py-2 text-sm text-white hover:bg-gray-100 hover:bg-gray-600 text-gray-200 hover:text-white"
+              className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
               onClick={() => signOut()}
             >
               Desconectar
